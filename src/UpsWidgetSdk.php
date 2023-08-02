b68abc66ab2cb2962cbd7edb96be802704421a6b
Michael Harmon <8215171@ups.com>
2023-08-02 14:56:45 -0400

    Added optional parameter for dynamic headers.

--- src/UpsWidgetSdk.php
+++ src/UpsWidgetSdk.php
@@ -11,6 +11,46 @@ class UPSSDK {
         $this->baseURL = $baseURL ?? 'https://onlinetools.ups.com/security/v1/oauth/token';
     }
 
+    public function generateToken($sessionId, $curlOptions = null) {
+        //Is sessionId gauranteed to not be null?
+        //What will curlOptions look like?
+        $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode(['sessionid' => $sessionId]));
+
+        $curl = curl_init();
+
+        if($curlOptions == null){
+            $curlOptions = array(
+                CURLOPT_URL => $this->baseURL,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $postData,
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
+                    'Content-Type: application/x-www-form-urlencoded',
+                    'Cookie: ups_language_preference=fr_CA'
+                ),
+            );
+        }
+
+        curl_setopt_array($curl, $curlOptions);
+
+        $response = curl_exec($curl);
+        $error = curl_error($curl);
+        curl_close($curl);
+
+        if($error){
+            return $error;
+        } else {
+            return $response;
+        }
+    }
+
+    /* OLD
     public function generateToken($sessionId) {
         $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode(['sessionid' => $sessionId]));
 
@@ -40,6 +80,6 @@ class UPSSDK {
         } else {
             return $response;
         }
-    }
+    }*/
 }
 
