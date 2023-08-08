0863facf73bb1449e6c41239763d25f2f5a47453
Michael Harmon <8215171@ups.com>
2023-08-08 10:08:56 -0400

    Bugfix - Header formatting causing error.

--- src/UpsWidgetSdk.php
+++ src/UpsWidgetSdk.php
@@ -17,7 +17,6 @@ class UPSSDK {
 
         $curl = curl_init();
 
-        //Default Options
         $curlOptions = array(
                 CURLOPT_URL => $this->baseURL,
                 CURLOPT_RETURNTRANSFER => true,
@@ -35,7 +34,6 @@ class UPSSDK {
                 ),
             );
 
-        //Add Additional Headers
         if($additionalHeaders != null){
 
             if(array_is_list($additionalHeaders)){
@@ -45,7 +43,7 @@ class UPSSDK {
             } else {
                 $keys = array_keys($additionalHeaders);
                 for($i = 0; $size = count($keys), $i < $size; $i++){
-                    array_push($curlOptions[CURLOPT_HTTPHEADER], $keys[$i] . ': ' . $additionalHeaders[$keys[$i]]);
+                    array_push($curlOptions[CURLOPT_HTTPHEADER], $keys[$i] . ':' . $additionalHeaders[$keys[$i]]);
                 }
             }
         }
@@ -62,37 +60,5 @@ class UPSSDK {
             return $response;
         }
     }
-
-    /* OLD
-    public function generateToken($sessionId) {
-        $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode(['sessionid' => $sessionId]));
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->baseURL,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $postData,
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
-                'Content-Type: application/x-www-form-urlencoded',
-                'Cookie: ups_language_preference=fr_CA'
-            ),
-        ));
-        $response = curl_exec($curl);
-        $error = curl_error($curl);
-        curl_close($curl);
-
-        if ($error) {
-            return $error;
-        } else {
-            return $response;
-        }
-    }*/
 }
 
