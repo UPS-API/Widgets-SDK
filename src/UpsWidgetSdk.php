48d6241df3ae2875717144d85a3e35bd2357018e
Michael Harmon <8215171@ups.com>
2023-08-29 08:33:16 -0400

    Updated for unified conventions. Added optional query parameters'

--- src/UpsWidgetSdk.php
+++ src/UpsWidgetSdk.php
@@ -1,49 +1,30 @@
 <?php
 
 class UPSSDK {
-    private $baseURL;
-    private $clientId;
-    private $clientSecret;
+    private $baseURL = 'https://onlinetools.ups.com/security/v1/oauth/token';
 
-    public function __construct($clientId, $clientSecret, $baseURL = null) {
-        $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
-        $this->baseURL = $baseURL ?? 'https://onlinetools.ups.com/security/v1/oauth/token';
+    public function __construct() {
+
     }
-
-    /**
-     * Generates a token.
-     * 
-     * @access public
-     * @param string $sessionId
-     * @param array $customClaims
-     * @param array $additionalHeaders
-     * @return string
-     */
-    public function generateToken($sessionId = null, $customClaims = null, $additionalHeaders = null) {
-        $curl = curl_init();
-        $postData = '';
-
-        if($sessionId != null){ //Locator
-            if($customClaims != null){
-                $claims = array('sessionid' => $sessionId);
-                $size = count($customClaims);
-                $keys = array_keys($customClaims);
-                for($i = 0; $i < $size; $i++){
-                    $claims += array($keys[$i] => $customClaims[$keys[$i]]);
-                }
-                $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode($claims));
-            }
-            $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode(array('sessionid' => $sessionId)));
-        } else { //Returns
-            if($customClaims != null){
-                $postData = 'grant_type=1&custom_claims=' . urlencode(json_encode($customClaims));
-            } else {
-                $postData = 'grant_type=1';
-            }
-        }
-
-        $curlOptions = array(
+	
+	public function generateToken($clientId, $clientSecret, $headers = null, $postData = null, $queryParams = null){
+		$curl = curl_init();
+		$body = '';
+		$response = '';
+		
+		if($postData != null){
+			$claims = array();
+			$size = count($postData);
+			$keys = array_keys($postData);
+			for($i = 0; $i < $size; $i++){
+				$claims += array($keys[$i] => $postData[$keys[$i]]);
+			}
+			$body = 'grant_type=client_credentials&custom_claims=' . urlencode(json_encode($claims));
+		} else {
+			$body = 'grant_type=client_credentials';
+		}
+		
+		$curlOptions = array(
             CURLOPT_URL => $this->baseURL,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
@@ -52,30 +33,29 @@ class UPSSDK {
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $postData,
+            CURLOPT_POSTFIELDS => $body,
             CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
+                'Authorization: Basic ' . base64_encode($clientId . ':' . $clientSecret),
                 'Content-Type: application/x-www-form-urlencoded',
                 'Cookie: ups_language_preference=en_US'
             ),
         );
-
-        //Add Additional Http Headers
-        if($additionalHeaders != null){
-            $size = count($additionalHeaders);
-            if(array_is_list($additionalHeaders)) {
-                for($i = 0; $i < $size; $i++) {
-                    array_push($curlOptions[CURLOPT_HTTPHEADER], $additionalHeaders[$i]);
-                }
-            } else {
-                $keys = array_keys($additionalHeaders);
-                for($i = 0; $i < $size; $i++) {
-                    array_push($curlOptions[CURLOPT_HTTPHEADER], $keys[$i] . ':' . $additionalHeaders[$keys[$i]]);
-                }
-            }
-        }
-
-        curl_setopt_array($curl, $curlOptions);
+		
+		if($headers != null){
+			$size = count($headers);
+			$keys = array_keys($headers);
+			for($i = 0; $i < $size; $i++){
+				array_push($curlOptions[CURLOPT_HTTPHEADER], $keys[$i] . ':' . $headers[$keys[$i]]);
+			}
+		}
+		
+		if($queryParams != null){
+			foreach($queryParams as $key => $value){
+				$this->baseURL = $this->baseURL . '?' . $key . '=' . $value;
+			}
+		}
+		
+		curl_setopt_array($curl, $curlOptions);
         $response = curl_exec($curl);
         $error = curl_error($curl);
         curl_close($curl);
@@ -85,5 +65,5 @@ class UPSSDK {
         } else {
             return $response;
         }
-    }
+	}
 }
